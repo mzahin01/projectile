6f65b28f4dadea3fce72a44f66537e956c5e053d
mzahin01 <minhazahin@gmail.com>
2025-07-02 02:47:03 +0600

    cow running

--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,10 +1,18 @@
 import 'dart:async';
 import 'package:flame/components.dart';
 
-class Player extends SpriteComponent with HasGameReference {
+class Player extends SpriteAnimationComponent with HasGameReference {
   @override
   FutureOr<void> onLoad() async {
-    sprite = await game.loadSprite('cowSprite.png');
+    animation = await game.loadSpriteAnimation(
+      'cowSprite.png',
+      SpriteAnimationData.sequenced(
+        amount: 6, // Total number of frames in your animation
+        amountPerRow: 3, // The number of frames per row
+        stepTime: .1, // The time to display each frame
+        textureSize: Vector2(250, 160), // The size of a single frame
+      ),
+    );
     return super.onLoad();
   }
 }
